c2bf3565e3c7a231e6377676aab2b673d6ed611d
bglee <deptno@gmail.com>
2020-01-05 21:27:24 +0900

    feat: add atrule option

--- src/index.ts
+++ src/index.ts
@@ -14,39 +14,61 @@ const pkg = JSON.parse(
 
 program
   .version(pkg.version)
-  .requiredOption('-o, --omit <rule...>', 'rule is css selector you want to strip', (arg, all = []) =>
-    [...all, new RegExp(arg)]
+  .option(
+    '-r, --rule <rules...>',
+    'rule',
+    (arg, all = []) => [...all, new RegExp(arg)],
+  )
+  .option(
+    '-a, --atrule <atrules...>',
+    '@rule',
+    (arg, all = []) => [...all, new RegExp(arg)],
   )
 
-const omitRules = program.parse(process.argv).omit
-
-log(omitRules)
+const omitRules = program.parse(process.argv).rule || []
+const omitAtrules = program.parse(process.argv).atrule || []
 const css = readFileSync(0).toString()
 const ast = tree.parse(css)
 const omitSet = new Set<string>()
 let ruleCount = 0
 
-tree.walk(ast, (node, item, list) => {
-  if (node.type === 'Rule') {
-    ruleCount++
-    tree.walk(node, ((node1, item1, list1) => {
-      if (node1.type === 'ClassSelector') {
-        if (omitRules.some(r => r.test(node1.name))) {
-          if (omitSet.has(node1.name)) {
-            return
-          }
-          omitSet.add(node1.name)
+log(omitRules)
 
-          try {
-            list.remove(item)
-          } catch (e) {
-            // node is already removed
+tree.walk(
+  ast,
+  (node, item, list) => {
+    if (node.type === 'Rule') {
+      ruleCount++
+      tree.walk(node, ((node1, item1, list1) => {
+        if (node1.type === 'ClassSelector') {
+          if (omitRules.some(r => r.test(node1.name))) {
+            if (omitSet.has(node1.name)) {
+              return
+            }
+            omitSet.add(node1.name)
+
+            try {
+              list.remove(item)
+            } catch (e) {
+              // node is already removed
+            }
           }
         }
+      }))
+    } else if (node.type === 'Atrule') {
+      const atruleCss = tree.generate(node)
+
+      if (omitAtrules.some(r => r.test(atruleCss))) {
+        log(
+          atruleCss
+            .slice(0, atruleCss.indexOf('{'))
+            .trim()
+        )
+        list.remove(item)
       }
-    }))
-  }
-})
+    }
+  },
+)
 const nextCss = tree.generate(ast)
 
 console.error(`rule ${ruleCount - omitSet.size}: ${ruleCount} - ${omitSet.size}`)
